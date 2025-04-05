554d1fa0c69610503a4bad10144a13e3497b819e
gosurvashi29 <urvashigoswami350@gmail.com>
2025-04-05 23:46:25 +0530

    added cart functionalities

--- app.js
+++ app.js
@@ -18,24 +18,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5bab316ce0a7c75f783cb8a8')
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    'mongodb+srv://admin:admin1234@cluster0.0lljc5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-  )
+mongoose.connect('mongodb://admin:admin1234@ac-xeqrfuv-shard-00-00.0lljc5f.mongodb.net:27017,ac-xeqrfuv-shard-00-01.0lljc5f.mongodb.net:27017,ac-xeqrfuv-shard-00-02.0lljc5f.mongodb.net:27017/?ssl=true&retryWrites=true&w=majority&appName=Cluster0&authSource=admin&replicaSet=atlas-sxwo5o-shard-0', {
+  useNewUrlParser: true,   // Add this option
+  useUnifiedTopology: true // Recommended, explained below
+})
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
